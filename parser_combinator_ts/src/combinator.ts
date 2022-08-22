367ab88fa4ba8b6758a65325c0d9776b7ff008fe
Frank Moreno <frankmoreno1993@gmail.com>
2022-08-21 23:25:22 -0500

    ts: delimited and delimitedLeft

--- parser_combinator_ts/src/combinator.ts
+++ parser_combinator_ts/src/combinator.ts
@@ -52,3 +52,30 @@ export function map<A, B>(parser: Parser<A>, fn: (val: A) => B): Parser<B> {
     return !res.success ? (res as Failure) : res.ctx.success(fn(res.value));
   };
 }
+
+export function delimited<T, L, R>(
+  left: Parser<L>,
+  parser: Parser<T>,
+  right: Parser<R>
+): Parser<T> {
+  return (ctx) => {
+    const leftRes = left(ctx);
+    if (!leftRes.success) return leftRes as Failure;
+    const res = parser(leftRes.ctx);
+    if (!res.success) return res as Failure;
+    const rightRes = right(res.ctx);
+    if (!rightRes.success) return rightRes as Failure;
+    return rightRes.ctx.success(res.value);
+  };
+}
+
+export function delimitedLeft(
+  left: Parser<string>,
+  parser: Parser<any>
+): Parser<any> {
+  return (ctx) => {
+    const leftRes = left(ctx);
+    if (!leftRes.success) return leftRes as Failure;
+    return parser(leftRes.ctx);
+  };
+}
