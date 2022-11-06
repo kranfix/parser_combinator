b6272a9492579d031395195d954395d9796983ab
Frank Moreno <frankmoreno1993@gmail.com>
2022-11-06 13:25:55 -0500

    json_nom: ident helper

--- json_nom/src/commons.rs
+++ json_nom/src/commons.rs
@@ -1,4 +1,8 @@
-use nom::{character::complete::char, multi::many0, IResult, Parser};
+use nom::{
+  character::complete::{alpha1, alphanumeric0, char},
+  multi::many0,
+  IResult, Parser,
+};
 
 pub fn whitespace(input: &str) -> IResult<&str, ()> {
   let ws = char(' ').or(char('\n')).or(char('\r')).or(char('\t'));
@@ -6,9 +10,15 @@ pub fn whitespace(input: &str) -> IResult<&str, ()> {
   Ok((input, ()))
 }
 
+pub fn ident(input: &str) -> nom::IResult<&str, String> {
+  let (input, (first, second)) = alpha1.and(alphanumeric0).parse(input)?;
+  let val = format!("{}{}", first, second);
+  Ok((input, val))
+}
+
 #[cfg(test)]
 mod test {
-  use super::whitespace;
+  use super::{ident, whitespace};
 
   #[test]
   fn whitespace_test() {
@@ -18,5 +28,23 @@ mod test {
 
     let (input, _) = whitespace(input).expect(msg);
     assert_eq!(input, "abc");
+  }
+
+  #[test]
+  fn test_ident() {
+    let (input, val) = ident("foo").unwrap();
+    assert_eq!(val, "foo");
+    assert_eq!(input, "");
+
+    let res = ident(input);
+    assert!(res.is_err());
+
+    let (input, val) = ident("foo(").unwrap();
+    assert_eq!(val, "foo");
+    assert_eq!(input, "(");
+
+    let (input, val) = ident("foo2A3dEz(").unwrap();
+    assert_eq!(val, "foo2A3dEz");
+    assert_eq!(input, "(");
   }
 }
