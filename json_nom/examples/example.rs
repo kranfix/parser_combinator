a5de71854eb83e7f2d04ac0a7b12d591b2ef59ba
Frank Moreno <frankmoreno1993@gmail.com>
2022-12-12 22:40:44 -0500

    added example

--- json_nom/examples/example.rs
+++ json_nom/examples/example.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use json_nom::j_value::JValue;
+
+fn main() {
+  let json = r#"
+      {
+        "a": [
+          1,
+          "hola",
+          {
+            "x": true
+          }
+        ],
+        "b": {
+          "y": [true, false, null]
+        }
+      }
+    "#;
+  let (_, jvalue) = JValue::parse(json).unwrap();
+  println!("{jvalue:?}");
+}
